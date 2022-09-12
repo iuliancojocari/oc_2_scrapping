757557a8b945e53dca11a4be628a0ccf1193dbf3
IulianK <iuliancojocari42@gmail.com>
2022-09-12 22:14:25 +0200

    Débogage : pb encodage .csv + voir transformation données (etl)

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from msilib.schema import Directory
-from ntpath import join
-from unicodedata import category
+from dataclasses import replace
 import requests
 from bs4 import BeautifulSoup
 import csv
 from urllib.parse import urljoin
 import os
-
+import re
 
 url = "http://books.toscrape.com/index.html"
 
@@ -55,7 +53,7 @@ def get_category_pages(cat_url):
 
         if not cat_url:
             break
-        print(cat_url)
+        
         pages.append(cat_url)
 
     return pages
@@ -75,27 +73,36 @@ def get_book_urls(cat_url):
             book_urls.append(urljoin(cat_url, book_url))
 
     return book_urls
+
+def create_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+def download_cat_img(img_file, book, image_url):
+    with open(img_file + '.jpg', 'wb') as handler:
+        image_complete_url = urljoin(book, image_url)
+        response = requests.get(image_complete_url)
+        handler.write(response.content)
         
 def get_book_data(site_home_url):
     categories = get_categories(site_home_url)
     
-    for cat_name, cat_url in categories.items():
+    print("Processing...")
 
-        #category_pages = get_category_pages(cat_url)
-        #print(cat_name + " --- " + str(len(category_pages)) + " pages")
-        
-        #for category_page in category_pages:
-        print(cat_url)
+    for cat_name, cat_url in categories.items():
+        print('\n')
+        print('**********************************************')
+        print('         Category Name - ' + cat_name + '     ')
+        print('**********************************************')
+
         books = get_book_urls(cat_url)
         
+        cat_directory = "data/" + cat_name.lower() + "/"
+        create_directory(cat_directory)
 
-        directory = "data"
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+        filename = cat_directory + cat_name.lower() + '.csv'
 
-        filename = 'data/' + cat_name + '.csv'
-
-        with open(filename, 'a', newline='', encoding='utf-8') as csvfile:
+        with open(filename, 'a', encoding='utf-8', newline='') as csvfile:
 
             fieldnames = ['product_page_url', 
                                     'title', 
@@ -106,40 +113,47 @@ def get_book_data(site_home_url):
                                     'price_including_tax',
                                     'number_available',
                                     'review_rating']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                                    
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
 
             if os.stat(filename).st_size == 0:
                 writer.writeheader()
 
             for book in books:
-                
                 soup = get_soup(book)
 
                 product_page_url        = book
                 title                   = soup.h1.text
                 category                = soup.find('ul', {'class':'breadcrumb'}).select('li')[2].find('a').text
-                    
-                try:
-                    product_description     = soup.find(id='product_description').find_next_sibling('p').text
-                except:
-                    product_description = 'N/A'
+                product_description     = soup.find(id='product_description').find_next_sibling('p').string
                 image_url               = soup.find('div', {'class' : 'item'}).find('img').get('src')
                 universal_product_code  = soup.find('table', {'class': 'table'}).select('td')[0].text
                 price_including_tax     = soup.find('table', {'class': 'table'}).select('td')[2].text
                 number_available        = soup.find('table', {'class': 'table'}).select('td')[5].text
                 review_rating           = soup.find('table', {'class': 'table'}).select('td')[6].text
 
-                    
+                print('Book Name - ' + title)
+
+
                 writer.writerow({   'product_page_url'          : product_page_url, 
                                     'title'                     : title,
                                     'category'                  : category,
-                                    'product_description'       : product_description,
+                                    'product_description'       : product_description.encode('utf-8'),
                                     'image_url'                 : image_url,
                                     'universal_product_code'    : universal_product_code,
                                     'price_including_tax'       : price_including_tax,
                                     'number_available'          : number_available,
                                     'review_rating'             : review_rating
                                     })
+
+
+                img_directory = cat_directory + '/img/'
+                create_directory(img_directory)
+
+                img_file = title.replace(' ', '_')
+                img_file = img_directory +  re.sub(r"[^a-zA-Z0-9_]","",img_file)
+
+                download_cat_img(img_file, book, image_url)              
 
 
 get_book_data(url)
